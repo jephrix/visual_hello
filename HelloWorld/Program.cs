1424a2c57bf2554870066f1f00ec0d9bacef7c04
jephrix <jephrix@gmail.com>
2016-11-04 19:22:59 +0900

    Code is mine

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -10,6 +10,11 @@ namespace HelloWorld
   {
     static void Main(string[] args)
     {
+      Console.WriteLine("Hello World");
+      Console.WriteLine("World is mine");
+      Console.WriteLine("Earch is mine");
+      Console.WriteLine("Luna is mine");
+      Console.WriteLine("Code is mine");
     }
   }
 }
